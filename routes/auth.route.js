d072282d8dc7b398bd81037d4223222957aedbde
alx_27 <didieralejandromejia83@gmail.com>
2024-01-01 16:39:44 -0500

    Validation for json body

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,23 @@
 import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
 import { body } from "express-validator";
+import { validationResultExpress } from "../middlewares/validationResultExpress.js";
 const router = express.Router();
 
-router.post("/login", login)
+router.post("/login", [
+    body("email", "Not correct email").trim().isEmail().normalizeEmail(),
+    body("password", "Min 6 characters").trim().isLength({min:6}),
+], validationResultExpress, login)
 
 router.post("/register", [
-    body("email", "Not correct email").isEmail().normalizeEmail(),
-], register)
+    body("email", "Not correct email").trim().isEmail().normalizeEmail(),
+    body("password", "Min 6 characters").trim().isLength({min:6}),
+    body("password", "Invalid password").custom((value, {req}) => {
+        if(value !== req.body.repassword){
+            throw new Error("The passwords don't match");
+        }
+        return value;
+    }),
+], validationResultExpress, register)
 
 export default router;
