41396eb3305ae016c84e30d546f4bec07dd9f1ca
alx_27 <didieralejandromejia83@gmail.com>
2024-01-02 19:07:36 -0500

    Endpoint for create new link

--- controllers/link.controller.js
+++ controllers/link.controller.js
@@ -1,3 +1,26 @@
-export const getLinks = (req, res) => {
-    return res.json({ok: true});
+import { Link } from "../models/Link.js"
+import { nanoid } from "nanoid";
+
+export const getLinks = async (req, res) => {
+    try{
+        const links = await Link.find({uid: req.uid});
+
+        return res.json({links});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({error: "Server error"});
+    }
+};
+
+export const createLink = async (req, res) => {
+    try{
+        const { longLink } = req.body;
+
+        const link = new Link({longLink, nanoLink: nanoid(6), uid: req.uid})
+
+        return res.json({longLink})
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({error: "Server error"});
+    }
 }
--- routes/link.route.js
+++ routes/link.route.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { getLinks } from "../controllers/link.controller.js";
+import { createLink, getLinks } from "../controllers/link.controller.js";
+import { requireToken } from "../middlewares/requireToken.js";
 const router = Router();
 
 // GET    /api/v1/links       all links
-router.get("/", getLinks);
+router.get("/", requireToken, getLinks);
 
 // GET    /api/v1/links/:id   single link
 //POST    /api/v1/links       create link
+router.post("/", requireToken, createLink);
 //PATCH   /api/v1/links/:id   update link
 //DELETE   /api/v1/links/:id  remove link
 
