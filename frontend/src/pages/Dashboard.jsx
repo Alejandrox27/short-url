556949f093f85aa5bebfe37a967157392c34632e
alx_27 <didieralejandromejia83@gmail.com>
2024-01-14 20:17:17 -0500

    Add messages for urls

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,21 +3,72 @@
 import LinkCard from "../components/LinkCard";
 import "../linkCard.css";
 import "../dashboard.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CircularProgress } from "@mui/material";
 
 const Dashboard = () => {
 
-    const [longlink, setLonglink] = useState("");
+    const [longLink, setLongLink] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
 
     const links = JSON.parse(localStorage.getItem("ulinks"));
+
+    useEffect(() => {
+        setTimeout(() => {
+            setError(null);
+            setSuccess(null);
+        }, 3000);
+    }, [error, success]);
     
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault();
 
         try{
-            
+            setLoading(true);
+            let res = await fetch("http://localhost:5000/api/v1/auth/refresh",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+
+            let data = await res.json();
+            if (data.error){
+                console.log(data);
+                setError(data.error.msg);
+            }
+
+            const token = data.token;
+
+            res = await fetch("http://localhost:5000/api/v1/links", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    longLink
+                }),
+                credentials: "include"
+            });
+
+            data = await res.json();
+            if (data.error || data.errors){
+                console.log(data);
+                setError(data.errors[0].msg);
+                return
+            };
+
+            setSuccess("URL added successfully")
+
         }catch(error){
             console.log(error);
+        } finally{
+            setLoading(false);
         }
     }
 
@@ -31,16 +82,27 @@ const Dashboard = () => {
                         <input 
                         type="text"
                         placeholder="longlink"
-                        name="longlink"
+                        name="longLink"
                         id="longlink"
                         autoComplete="off"
                         className="input-add-longlink"
                         onChange={(e) => {
-                            setLonglink(e.target.value)
+                            setLongLink(e.target.value)
                         }}
-                        value={longlink}
+                        value={longLink}
                         />
-                        <button type="submit" className="button-add-link">Add</button>
+                        <div className="add-button-and-loading">
+                            <button type="submit" className="button-add-link">Add</button>
+                            {
+                                loading && <CircularProgress />
+                            }
+                            {
+                                error && <p className="text-danger">{error}</p>
+                            }
+                            {
+                                success && <p className="text-success">{success}</p>
+                            }
+                        </div>
                     </form>
                 </div>
                 <ul className="links-list">
